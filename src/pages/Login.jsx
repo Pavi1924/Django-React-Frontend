106795163730e2ed5d746e6c612c9b3c6b8f46d6
Pavi1924 <pavithrav242@gmail.com>
2025-01-28 13:54:44 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import Form from "../components/Form"
+
+function Login() {
+    return <Form route="https://django-react-mumx.onrender.com/api/token/" method="login" />
+}
+
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import Form from "../components/Form"
+
+function Register() {
+    return <Form route="https://django-react-mumx.onrender.com/api/user/register/" method="register" />
+    
+}
+
+
+export default Register
